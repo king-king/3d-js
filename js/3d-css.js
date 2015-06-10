9ddfd8399611ec6d387269f38d1a29dbd1d7f79d
WQ <niuyiwangqun@sohu.com>
2015-06-10 11:33:58 +0800

    3d矩阵

--- js/3d-css.js
+++ js/3d-css.js
@@ -25,8 +25,7 @@
         var cos = Math.cos, sin = Math.sin;
         a = a * Math.PI / 180;
         return [
-            [1 + (1 - cos( a ))( x * x - 1 ),
-                z * sin( a ) + x * y * (1 - cos( a )), -y * sin( a ) + x * z * (1 - cos( a )), 0],
+            [1 + (1 - cos( a )) * ( x * x - 1 ), z * sin( a ) + x * y * (1 - cos( a )), -y * sin( a ) + x * z * (1 - cos( a )), 0],
             [-z * sin( a ) + x * y * (1 - cos( a )), 1 + (1 - cos( a )) * (y * y - 1), x * sin( a ) + y * z * (1 - cos( a )), 0],
             [y * sin( a ) + x * z * (1 - cos( a )), -x * sin( a ) + y * z * (1 - cos( a )), 1 + (1 - cos( a )) * (z * z - 1), 0],
             [0, 0, 0, 1]
@@ -54,12 +53,39 @@
         return nmatrix.join();
     };
 
+    function combin() {
+        var result = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ];
+        for ( var i = 0; i < arguments.length; i++ ) {
+            result = mul( result, arguments[i] );
+        }
+        return result;
+    }
+
+    function mul( m1, m2 ) {
+        var re = [];
+        for ( var r = 0; r < m1.length; r++ ) {
+            re[r] = [];
+            for ( var c = 0; c < m1[r].length; c++ ) {
+                re[r][c] = 0;
+                for ( var k = 0; k < m1.length; k++ ) {
+                    re[r][c] += m1[r][k] * m2[k][c];
+                }
+            }
+        }
+        return re;
+    }
 
     window._3d = {
         perspectiveM : perspectiveM,
         translate3dM : translate3dM,
         rotate3dM : rotate3dM,
-        scale3dM : scale3dM
+        scale3dM : scale3dM,
+        mul : mul
     }
 
 })();
