b542adb1098fbbd2449eb90f570d51d4fe88d132
WQ <niuyiwangqun@sohu.com>
2015-11-02 16:24:47 +0800

    提炼代码

--- example/cube/cube.js
+++ example/cube/cube.js
@@ -1,6 +1,24 @@
 /**
  * Created by WQ on 2015/11/2.
  */
+function Block( type , row , col ) {
+    var el = element( "div" , {
+        classList : [ "block" , type ] ,
+        innerHTML : "<div class='block-inner'></div>"
+    } , cubeWrapper );
+    Blocks.push( el );
+    css( el , {
+        "margin-left" : initPos[ row ][ col ][ 0 ] + "px" ,
+        "margin-top" : initPos[ row ][ col ][ 1 ] + "px"
+    } );
+    el.origin = initOrigin[ row ][ col ];
+    /**
+     *最复杂的一点，就是给每个block附上旋转的数据以及旋转的操作
+     * 每个block只可能绕着两个轴来旋转，找出这两个点，给它附上一个独一无二的标记
+     */
+    return el;
+}
+
 function initCube() {
     // 如果sixFaces里面有数据，也就是说有dom元素，则只需要重新摆放即可，否则需要生成
     loopObj( sixFaces , function ( type , face ) {
@@ -13,17 +31,7 @@ function initCube() {
                     // 恢复颜色
                     el.className = "block " + type;
                 } else {
-                    el = element( "div" , {
-                        classList : [ "block" , type ] ,
-                        innerHTML : "<div class='block-inner'></div>"
-                    } , cubeWrapper );
-                    Blocks.push( el );
-                    css( el , {
-                        "margin-left" : initPos[ row ][ col ][ 0 ] + "px" ,
-                        "margin-top" : initPos[ row ][ col ][ 1 ] + "px"
-                    } );
-                    el.origin = initOrigin[ row ][ col ];
-                    face.elements[ index ] = el;
+                    face.elements[ index ] = el = Block( type , row , col );
                 }
                 // 摆放位置
                 el.matrix = face.transform;
